81d9cafaf147d5488387ce978d151941c3a079a4
max <max.thomas@jhu.edu>
2013-05-16 08:07:54 -0400

    cleaned up test

--- concrete-java/src/test/java/edu/jhu/concrete/io/ProtocolBufferWriterTest.java
+++ concrete-java/src/test/java/edu/jhu/concrete/io/ProtocolBufferWriterTest.java
@@ -2,6 +2,8 @@ package edu.jhu.concrete.io;
 
 //import static org.junit.Assert.fail;
 
+import static org.junit.Assert.assertEquals;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -9,16 +11,13 @@ import java.io.FileOutputStream;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import edu.jhu.concrete.Concrete.Communication;
-import edu.jhu.concrete.util.IdUtil;
+import edu.jhu.concrete.Concrete.CommunicationGUID;
+import edu.jhu.concrete.Concrete.UUID;
 import edu.jhu.concrete.util.ProtoFactory;
 
 public class ProtocolBufferWriterTest {
-    
-    private static final Logger logger = LoggerFactory.getLogger(ProtocolBufferWriterTest.class);
     
     @Before
     public void setUp() throws Exception {
@@ -40,8 +39,8 @@ public class ProtocolBufferWriterTest {
         FileOutputStream fos = new FileOutputStream(output);
         Communication comm = new ProtoFactory().generateMockCommunication();
         
-        logger.info("Writing ID: " + IdUtil.uuidToString(comm.getUuid()));
-        logger.info("Writing GUID: " + comm.getGuid().getCommunicationId());
+        UUID id = comm.getUuid();
+        CommunicationGUID guid = comm.getGuid();
         ProtocolBufferWriter pbw = new ProtocolBufferWriter(fos);
         pbw.write(comm);
         
@@ -49,8 +48,10 @@ public class ProtocolBufferWriterTest {
         ProtocolBufferReader pbr = new ProtocolBufferReader(fis, Communication.class);
         Communication readComm = (Communication) pbr.next();
         
-        logger.info("Got a communication: " + IdUtil.uuidToString(readComm.getUuid()));
-        logger.info("Got GUID: " + readComm.getGuid().getCommunicationId());
+        UUID readId = readComm.getUuid();
+        CommunicationGUID readGuid = readComm.getGuid();
+        assertEquals(id, readId);
+        assertEquals(guid, readGuid);
     }
 
 }
