838839fa32f0be192b76ef94a75ef105dab9e59f
Mark Dredze <mdredze@cs.jhu.edu>
2013-07-10 23:10:52 -0400

    updated python compilation script

--- compile_python.sh
+++ compile_python.sh
@@ -1,3 +1,3 @@
-mkdir concrete-python/target 2>/dev/null
-protoc --proto_path=./concrete-protobufs/src/main/proto/ --python_out=./concrete-python/target concrete-protobufs/src/main/proto/concrete* 
+mkdir -p concrete-python/target 
+protoc --proto_path=./concrete-protobufs/src/main/proto/ --python_out=./concrete-python/target concrete-protobufs/src/main/proto/*.proto
 cp -R ./concrete-python/src/* ./concrete-python/target/
