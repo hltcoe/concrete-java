abb12aa396bbac31b5accc5125e0eb3f04dd3200
Mark Dredze <mdredze@cs.jhu.edu>
2013-07-18 15:03:08 -0400

    This compile script is no longer needed after the last checkin.

--- compile_python.sh
+++
@@ -1,3 +0,0 @@
-mkdir -p concrete-python/target 
-protoc --proto_path=./concrete-protobufs/src/main/proto/ --python_out=./concrete-python/target concrete-protobufs/src/main/proto/*.proto
-cp -R ./concrete-python/src/* ./concrete-python/target/
