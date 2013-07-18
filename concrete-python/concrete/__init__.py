4d35f96b84c7ab918b8c7cf1b429fcc3a537c82c
max thomas <maxjthomas@gmail.com>
2013-07-18 14:46:03 -0400

    Added python protobuf skeleton

---
+++ concrete-python/concrete/__init__.py
@@ -0,0 +1,17 @@
+"""
+Concrete Core Python Package.
+
+This package defines the basic data structures used by the Concrete
+architecture, along with utility classes and functions for processing
+those data structures.
+"""
+
+# The Concrete protobuf data structures (Communication, Tokenization,
+# Vertex, Edge, etc.)
+# from concrete.proto import *
+from concrete.proto import *
+
+# Utility functions & classes
+from concrete.id_util import *
+# from rebar2.lid_util import *
+
