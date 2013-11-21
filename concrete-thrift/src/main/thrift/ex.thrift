9e31d9f37d957895f510f1313021f1bc50451b24
Max Thomas <max.thomas@jhu.edu>
2013-11-20 22:15:15 -0500

    refactor documents to communications; add exception thrift def

---
+++ concrete-thrift/src/main/thrift/ex.thrift
@@ -0,0 +1,6 @@
+namespace java edu.jhu.hlt.concrete
+
+exception RebarThriftException {
+  1: string message
+  2: optional binary serEx
+}
--- concrete-thrift/src/main/thrift/stage.thrift
+++ concrete-thrift/src/main/thrift/stage.thrift
@@ -1,15 +1,24 @@
+include "ex.thrift"
+
 namespace java edu.jhu.hlt.concrete
+
+enum StageType {
+  LANG_ID = 1
+  LANG_PRED = 2
+}
 
 struct Stage {
   1: string name
   2: string desc
   3: i32 createTime
   4: set<string> dependencies
+  5: StageType type
 }
 
 service StageHandler {
-  bool stageExists(1: string stageName)
-  void createStage(1: Stage stage)
-  set<Stage> getStages()
+  bool stageExists(1: string stageName) throws (1: ex.RebarThriftException exc)
+  void createStage(1: Stage stage) throws (1: ex.RebarThriftException exc)
+  set<Stage> getStages() throws (1: ex.RebarThriftException exc)
+  i32 getAnnotatedDocumentCount(1: Stage stage) throws (1: ex.RebarThriftException exc)
   //TODO: delete
 }
