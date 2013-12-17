8af1f51413e6c669832955e8107552f98afbbdb2
max thomas <max.thomas@jhuapl.edu>
2013-12-17 18:14:15 -0500

    Modify Util library to utilize pimp-my-library pattern.

--- util/src/main/scala/edu/jhu/hlt/concrete/util/Util.scala
+++ util/src/main/scala/edu/jhu/hlt/concrete/util/Util.scala
@@ -4,6 +4,7 @@
 package edu.jhu.hlt.concrete.util
 
 import edu.jhu.hlt.concrete.{UUID, TextSpan}
+import scala.language.implicitConversions
 
 /**
  * @author max
@@ -14,8 +15,14 @@ object ConcreteUtil {
     val jUuid = java.util.UUID.randomUUID()
     new UUID(jUuid.getMostSignificantBits, jUuid.getLeastSignificantBits)
   }
-  
-  def getTextSpanText (text: String, span: TextSpan) : String = {
-    text.substring(span.getStart(), span.getEnding())
-  }
 }
+
+class SuperTextSpan(textSpan: TextSpan) {
+  def getSpanText(text: String) : String = {
+    text.substring(textSpan.getStart, textSpan.getEnding)
+  }
+
+  implicit def superTextSpan(textSpan: TextSpan) = new SuperTextSpan(textSpan)
+}
+
+
