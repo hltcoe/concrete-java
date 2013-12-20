4ff9b6e5f6eb800bce144c2d3459bd896e5aafdd
Max Thomas <max.thomas@jhu.edu>
2013-12-19 20:45:34 -0500

    tweak scaladoc; add Util readme.md.

---
+++ util/README.md
@@ -0,0 +1,5 @@
+# concrete-util #
+
+## Introduction ##
+
+This is a Java utility library based around Concrete that provides some useful utility methods.
--- util/src/main/scala/edu/jhu/hlt/concrete/util/Util.scala
+++ util/src/main/scala/edu/jhu/hlt/concrete/util/Util.scala
@@ -6,17 +6,27 @@ package edu.jhu.hlt.concrete.util
 import edu.jhu.hlt.concrete.{UUID, TextSpan}
 
 /**
- * @author max
- *
- */
+  * Utility code for working with Concrete.
+  * 
+  * @author max
+  */
 object ConcreteUtil {
+  /**
+    * Return a randomly generated Concrete [[UUID]] object. 
+    */
   def generateUUID : UUID = {
     val jUuid = java.util.UUID.randomUUID()
     new UUID(jUuid.getMostSignificantBits, jUuid.getLeastSignificantBits)
   }
 }
 
+/**
+  * A wrapper around [[TextSpan]] that provides additional utility methods for working with TextSpan objects. 
+  */
 class SuperTextSpan(textSpan: TextSpan) {
+  /**
+    * Returns a [[String]] that represents the text of the original document that this [[TextSpan]] object points to.
+    */
   def getSpanText(text: String) : String = {
     text.substring(textSpan.getStart, textSpan.getEnding)
   }
