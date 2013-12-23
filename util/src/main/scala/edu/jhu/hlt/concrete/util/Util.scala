9bf22314652945ac5d6a276eedc1537614851206
Max Thomas <max.thomas@jhuapl.edu>
2013-12-22 19:36:33 -0500

    Add a utility wrapper for UUIDs to make getting UUID strings easier.

--- util/src/main/scala/edu/jhu/hlt/concrete/util/Util.scala
+++ util/src/main/scala/edu/jhu/hlt/concrete/util/Util.scala
@@ -6,13 +6,13 @@ package edu.jhu.hlt.concrete.util
 import edu.jhu.hlt.concrete.{UUID, TextSpan}
 
 /**
-  * Utility code for working with Concrete.
+  * Utility code for working with Concrete objects.
   * 
   * @author max
   */
 object ConcreteUtil {
   /**
-    * Return a randomly generated Concrete [[UUID]] object. 
+    * Return a randomly generated Concrete `UUID` object. 
     */
   def generateUUID : UUID = {
     val jUuid = java.util.UUID.randomUUID()
@@ -25,11 +25,21 @@ object ConcreteUtil {
   */
 class SuperTextSpan(textSpan: TextSpan) {
   /**
-    * Returns a [[String]] that represents the text of the original document that this [[TextSpan]] object points to.
+    * Returns a `String` that represents the text of the original document that this [[TextSpan]] object points to.
     */
   def getSpanText(text: String) : String = {
     text.substring(textSpan.getStart, textSpan.getEnding)
   }
 }
 
-
+/**
+  * A wrapper around `UUID` that provides a method for getting a `String` representation of a `UUID`.
+  */
+class SuperUUID(uuid: UUID) {
+  /**
+    * Returns a `String` representation of the `UUID` encapsulated by this [[SuperUUID]] object.
+    */
+  def getUUIDString : String = {
+    new java.util.UUID(uuid.high, uuid.low).toString
+  }
+}
