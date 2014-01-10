648b59d99d8d93bf321bea8cd5bfff4d805e2aa4
Max Thomas <maxjthomas@gmail.com>
2014-01-10 18:42:18 -0500

    Fix up util to work with new scrooge setup.

--- util/src/main/scala/edu/jhu/hlt/concrete/util/Util.scala
+++ util/src/main/scala/edu/jhu/hlt/concrete/util/Util.scala
@@ -3,10 +3,11 @@
  */
 package edu.jhu.hlt.concrete.util
 
-import edu.jhu.hlt.concrete.{UUID, TextSpan, Communication}
+import edu.jhu.hlt.concrete.{Uuid, TextSpan, Communication}
 import java.io.{BufferedInputStream, FileInputStream, File}
 import org.apache.thrift.TDeserializer
 import org.apache.thrift.protocol.TBinaryProtocol
+import com.twitter.scrooge.BinaryThriftStructSerializer
 
 import scala.collection.JavaConversions._
 import scala.language.postfixOps
@@ -17,24 +18,27 @@ import scala.language.postfixOps
   * @author max
   */
 object ConcreteUtil {
-  private val DefaultDeserializer = new TDeserializer(new TBinaryProtocol.Factory())
 
   /**
-    * Use a default deserialization strategy to return a `Communication` object from a Java `File`.
+    * Use a default deserialization strategy to return a
+    * `Communication` object from a Java `File`.
+    *
     * @param file a Java `File` object representing a serialized Concrete `Communication`.
     * @return a `Communication` object.
     */
   def deserializeFile(file: File) : Communication = {
-    val comm = new Communication()
     val bis = new BufferedInputStream(new FileInputStream(file))
-    val byteArray = Stream.continually(bis.read).takeWhile(-1 !=).map(_.toByte).toArray
-    bis.close
-    DefaultDeserializer.deserialize(comm, byteArray)
-    comm
+    try {
+      BinaryThriftStructSerializer(Communication).fromInputStream(bis)
+    } finally {
+      bis.close
+    }
   }
 
   /**
-    * Use a default deserialization strategy to return a `Communication` object from a file path.
+    * Use a default deserialization strategy to return a
+    * `Communication` object from a file path.
+    *
     * @param path The path to a serialized Concrete `Communication`.
     * @return a `Communication` object.
     */
@@ -43,21 +47,31 @@ object ConcreteUtil {
   }
 
   /**
-    * Return a randomly generated Concrete `UUID` object.
+    * Return a randomly generated Concrete `Uuid` object.
     */
-  def generateUUID : UUID = {
-    new UUID(java.util.UUID.randomUUID().toString)
+  def generateUUID : Uuid = {
+    Uuid(java.util.UUID.randomUUID().toString)
   }
 }
 
 /**
-  * A wrapper around `TextSpan` that provides additional utility methods for working with TextSpan objects.
+  * A wrapper around `TextSpan` that provides additional utility
+  * methods for working with TextSpan objects.
   */
 class SuperTextSpan(textSpan: TextSpan) {
   /**
-    * Returns a `String` that represents the text of the original document that this `TextSpan` object points to.
+    * Returns a `String` that represents the text of the original
+    * document that this `TextSpan` object points to.
     */
   def getSpanText(text: String) : String = {
-    text.substring(textSpan.getStart, textSpan.getEnding)
+    text.substring(textSpan.start, textSpan.ending)
   }
+}
+
+/**
+  * A wrapper around `Communication` that provides additional utility
+  * methods.
+  */
+class SuperCommunication(comm: Communication) {
+
 }
