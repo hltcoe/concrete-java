748abfcb6aa5fa6c58ea9a0f1a6e17cf2aaab094
Max Thomas <maxjthomas@gmail.com>
2014-01-13 23:39:02 -0500

    Delete rebar specific thrift files.

--- thrift/ex.thrift
+++
@@ -1,8 +0,0 @@
-namespace java edu.jhu.hlt.concrete
-namespace py concrete.ex
-#@namespace scala edu.jhu.hlt.miser
-
-exception RebarThriftException {
-  1: string message
-  2: optional binary serEx
-}
