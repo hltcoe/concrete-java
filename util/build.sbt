d5e41e8268e573c99b86252d2fb8b363459a9557
Max Thomas <maxjthomas@gmail.com>
2014-01-13 23:23:10 -0500

    Update util to work with latest concrete.

--- util/build.sbt
+++ util/build.sbt
@@ -7,10 +7,10 @@ organization := "edu.jhu.hlt"
 scalaVersion := "2.10.3"
 
 libraryDependencies ++= Seq(
-  "edu.jhu.hlt" %% "concrete-core" % "2.0.3-SNAPSHOT",
-  "com.twitter" %% "scrooge-serializer" % "3.11.2"
+  "edu.jhu.hlt" % "concrete-core" % "2.0.3-SNAPSHOT"
+  // "com.twitter" %% "scrooge-serializer" % "3.11.2"
 )
 
 resolvers += "Local Maven Repository" at "file://"+Path.userHome+"/.m2/repository"
 
-publishTo := Some(Resolver.file("file",  new File(Path.userHome.absolutePath+"/.m2/repository")))
+// publishTo := Some(Resolver.file("file",  new File(Path.userHome.absolutePath+"/.m2/repository")))
--- util/src/main/scala/edu/jhu/hlt/concrete/util/Util.scala
+++ util/src/main/scala/edu/jhu/hlt/concrete/util/Util.scala
@@ -4,62 +4,28 @@
 package edu.jhu.hlt.concrete.util
 
 import edu.jhu.hlt.concrete.{TextSpan, Communication, CommunicationType}
-import java.io.{BufferedInputStream, FileInputStream, File}
-import org.apache.thrift.TDeserializer
-import org.apache.thrift.protocol.TBinaryProtocol
-import com.twitter.scrooge.BinaryThriftStructSerializer
-
-import scala.collection.JavaConversions._
-import scala.language.postfixOps
 
 /**
   * Utility code for working with Concrete objects.
   *
   * @author max
   */
-object ConcreteUtil {
+object Util {
   import scala.util.Random
   private val r = new Random
-
-  /**
-    * Use a default deserialization strategy to return a
-    * `Communication` object from a Java `File`.
-    *
-    * @param file a Java `File` object representing a serialized Concrete `Communication`.
-    * @return a `Communication` object.
-    */
-  def deserializeFile(file: File) : Communication = {
-    val bis = new BufferedInputStream(new FileInputStream(file))
-    try {
-      BinaryThriftStructSerializer(Communication).fromInputStream(bis)
-    } finally {
-      bis.close
-    }
-  }
-
-  /**
-    * Use a default deserialization strategy to return a
-    * `Communication` object from a file path.
-    *
-    * @param path The path to a serialized Concrete `Communication`.
-    * @return a `Communication` object.
-    */
-  def deserializeFile(path: String) : Communication = {
-    deserializeFile(new File(path))
-  }
 
   /**
     * A function that generates a mock `Communication`,
     * suitable for testing.
     * @return a `Communication` with a random doc ID, uuid,
-    a type of `CommunicationType.Other`, and some sample text.
+    a type of `CommunicationType.OTHER`, and some sample text.
     */
   def generateCommunication : Communication = {
     val rInt = r.nextInt
     val docIdStr = s"Communication_$rInt"
     val uuidStr = java.util.UUID.randomUUID.toString
     val text = "Lorem Ipsum. This is some sample text!"
-    Communication(docIdStr, uuidStr, CommunicationType.Other, text)
+    new Communication(docIdStr, uuidStr, CommunicationType.OTHER, text)
   }
 }
 
