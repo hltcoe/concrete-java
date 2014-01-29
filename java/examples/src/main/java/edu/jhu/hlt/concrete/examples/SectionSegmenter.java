4e2e87bb2cdf0888ecbae20f2cf2d8658d765f4f
max thomas <max.thomas@jhuapl.edu>
2014-01-29 18:32:02 -0500

    Additional work on java examples.

--- java/examples/src/main/java/edu/jhu/hlt/concrete/examples/SectionSegmenter.java
+++ java/examples/src/main/java/edu/jhu/hlt/concrete/examples/SectionSegmenter.java
@@ -1,5 +1,7 @@
 /**
- * 
+ * Copyright 2012-2014 Johns Hopkins University HLTCOE. All rights reserved.
+ * This software is released under the 2-clause BSD license.
+ * See LICENSE in the project root directory.
  */
 package edu.jhu.hlt.concrete.examples;
 
