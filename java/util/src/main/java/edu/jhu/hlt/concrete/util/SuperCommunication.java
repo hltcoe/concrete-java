564fea6ff422d01719f495e6ab1aa03a832cddb9
max thomas <maxjthomas@gmail.com>
2014-03-26 20:27:17 -0400

    Add some functionality to SuperCommunication

--- java/util/src/main/java/edu/jhu/hlt/concrete/util/SuperCommunication.java
+++ java/util/src/main/java/edu/jhu/hlt/concrete/util/SuperCommunication.java
@@ -13,6 +13,11 @@ import java.nio.file.Paths;
 import org.apache.thrift.TException;
 
 import edu.jhu.hlt.concrete.Communication;
+import edu.jhu.hlt.concrete.Section;
+import edu.jhu.hlt.concrete.SectionSegmentation;
+import edu.jhu.hlt.concrete.Sentence;
+import edu.jhu.hlt.concrete.SentenceSegmentation;
+import edu.jhu.hlt.concrete.Tokenization;
 
 /**
  * Wrapper around {@link Communication} to allow advanced functionality.
@@ -57,7 +62,96 @@ public class SuperCommunication {
     }
   }
   
+  /**
+   * Wrapper around {@link #writeToFile(Path, boolean)} that takes a {@link String} instead
+   * of a {@link Path}.
+   * 
+   * @see #writeToFile(Path, boolean)
+   * 
+   * @param pathString
+   * @param deleteExisting - whether to delete the file at path, if it exists.
+   * @throws ConcreteException if there are {@link IOException}s or {@link TException}s.
+   */
   public void writeToFile(String pathString, boolean deleteExisting) throws ConcreteException {
     this.writeToFile(Paths.get(pathString), deleteExisting);
+  }
+  
+  /**
+   * Get the first {@link SectionSegmentation} from this {@link Communication}.
+   * 
+   * If it is not set or is empty, throw a {@link ConcreteException}.
+   * 
+   * @return the first {@link SectionSegmentation} from the wrapped {@link Communication}
+   * @throws ConcreteException if {@link SectionSegmentation} is not set or is empty
+   */
+  public SectionSegmentation firstSectionSegmentation() throws ConcreteException {
+    if (this.comm.isSetSectionSegmentations() && this.comm.getSectionSegmentationsSize() > 0)
+      return this.comm.getSectionSegmentations().get(0);
+    else
+      throw new ConcreteException("Communication: " + this.comm.getUuid() + " does not have"
+          + " any SectionSegmentations.");
+  }
+  
+  /**
+   * Get the first {@link Section} from the first {@link SectionSegmentation} from 
+   * the wrapped {@link Communication}.
+   * 
+   * @return the first {@link Section} from the first {@link SectionSegmentation}
+   * @throws ConcreteException if there is no {@link Section} or {@link SectionSegmentation}
+   */
+  public Section firstSection() throws ConcreteException {
+    SectionSegmentation ss = this.firstSectionSegmentation();
+    if (ss.isSetSectionList() && ss.getSectionListSize() > 0)
+      return ss.getSectionList().get(0);
+    else
+      throw new ConcreteException("SectionSegmentation: " + ss.getUuid() + " does not have"
+          + " any Sections.");
+  }
+  
+  /**
+   * Get the first {@link SentenceSegmentation} from the first {@link Section} from
+   * the first {@link SectionSegmentation} of this wrapped {@link Communication}.
+   * 
+   * @return the first {@link SentenceSegmentation}
+   * @throws ConcreteException if there is no {@link SentenceSegmentation} or any prerequisites are missing
+   */
+  public SentenceSegmentation firstSentenceSegmentation() throws ConcreteException {
+    Section s = this.firstSection();
+    if (s.isSetSentenceSegmentation() && s.getSentenceSegmentationSize() > 0)
+      return s.getSentenceSegmentation().get(0);
+    else
+      throw new ConcreteException("Section: " + s.getUuid() + " does not have"
+          + " any Sentence Segmentations.");
+  }
+
+  /**
+   * Get the first {@link Sentence}.
+   * 
+   * @return the first {@link Sentence}
+   * @throws ConcreteException if there is no {@link Sentence}, or if any prerequisites are missing.
+   */
+  public Sentence firstSentence() throws ConcreteException {
+    SentenceSegmentation s = this.firstSentenceSegmentation();
+    if (s.isSetSentenceList() && s.getSentenceListSize() > 0)
+      return s.getSentenceList().get(0);
+    else
+      throw new ConcreteException("SentenceSegmentation: " + s.getUuid() + " does not have"
+          + " any Sentences.");
+  }
+
+  /**
+   * Get the first {@link Tokenization}.
+   * 
+   * @return the first {@link Tokenization}
+   * @throws ConcreteException if there is no {@link Tokenization}, or if any of the prerequisites are 
+   * missing.
+   */
+  public Tokenization firstTokenization() throws ConcreteException {
+    Sentence s = this.firstSentence();
+    if (s.isSetTokenizationList() && s.getTokenizationListSize() > 0)
+      return s.getTokenizationList().get(0);
+    else
+      throw new ConcreteException("Sentence: " + s.getUuid() + " does not have"
+          + " any Tokenizations.");
   }
 }
