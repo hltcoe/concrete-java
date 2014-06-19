e7458511bd88036bbcb322ba0ba1812b678507fe
max thomas <maxjthomas@gmail.com>
2014-06-19 17:40:03 -0400

    Initial cut at validation for tokenization collection.

--- java/validation/src/main/java/edu/jhu/hlt/concrete/validation/ValidatableTokenizationCollection.java
+++ java/validation/src/main/java/edu/jhu/hlt/concrete/validation/ValidatableTokenizationCollection.java
@@ -3,7 +3,10 @@
  */
 package edu.jhu.hlt.concrete.validation;
 
+import java.util.Iterator;
+
 import edu.jhu.hlt.concrete.Communication;
+import edu.jhu.hlt.concrete.Tokenization;
 import edu.jhu.hlt.concrete.TokenizationCollection;
 
 /**
@@ -28,7 +31,20 @@ public class ValidatableTokenizationCollection extends AbstractAnnotation<Tokeni
 
   @Override
   public boolean isValid() {
-    // TODO Auto-generated method stub
-    return false;
+    boolean initValidation = 
+        this.printStatus("Metadata must be set", this.annotation.isSetMetadata())
+        && this.printStatus("Metadata must be valid", new ValidatableMetadata(this.annotation.getMetadata()).isValid())
+        // Sections must exist.
+        && this.printStatus("Sections must exist", this.annotation.isSetTokenizationList())
+        // Section size must be >0.
+        && this.printStatus("Section size must be >0", this.annotation.getTokenizationListSize() > 0);
+    if (initValidation) {
+      Iterator<Tokenization> tokIter = this.annotation.getTokenizationListIterator();
+      boolean subValid = true;
+      while (subValid && tokIter.hasNext())
+        subValid = new ValidatableTokenization(tokIter.next()).isValid();
+      return subValid;
+    } else
+      return false;
   }
 }
