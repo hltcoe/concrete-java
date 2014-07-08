d9bb7775552a48c52e756647c3aecb2d123f2709
max thomas <maxjthomas@gmail.com>
2014-07-07 22:20:55 -0400

    Fixed an issue where utility method should have been switched to use processedContent.

--- util/src/main/java/edu/jhu/hlt/concrete/util/SuperTextSpan.java
+++ util/src/main/java/edu/jhu/hlt/concrete/util/SuperTextSpan.java
@@ -39,7 +39,7 @@ public class SuperTextSpan {
    */
   public String getText() {
     return c
-        .getText()
+        .getProcessedContent()
         .substring(ts.getStart(), ts.getEnding());
   }
 }
