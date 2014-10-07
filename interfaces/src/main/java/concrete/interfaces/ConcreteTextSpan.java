f4b853b35446471abdc56bc2d54c05d25e0f421b
Max Thomas <maxjthomas@gmail.com>
2014-10-07 16:00:53 -0400

    Add interfaces library.

---
+++ interfaces/src/main/java/concrete/interfaces/ConcreteTextSpan.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright 2012-2014 Johns Hopkins University HLTCOE. All rights reserved.
+ * See LICENSE in the project root directory.
+ */
+
+package concrete.interfaces;
+
+/**
+ * @author max
+ *
+ */
+public interface ConcreteTextSpan {
+  public int getStart();
+  public int getEnding();
+}
