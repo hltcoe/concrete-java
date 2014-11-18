2daa0ae7c94f9baffcd6bdc9f2bf501929b84650
Max Thomas <maxjthomas@gmail.com>
2014-11-18 15:48:32 -0500

    Add targz serialization API.

---
+++ util/src/main/java/edu/jhu/hlt/concrete/serialization/TarGzCommunicationIterator.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright 2012-2014 Johns Hopkins University HLTCOE. All rights reserved.
+ * See LICENSE in the project root directory.
+ */
+
+package edu.jhu.hlt.concrete.serialization;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Iterator;
+
+import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
+import org.apache.commons.compress.archivers.tar.TarArchiveInputStream;
+import org.apache.commons.compress.compressors.gzip.GzipCompressorInputStream;
+import org.apache.commons.compress.utils.IOUtils;
+
+import edu.jhu.hlt.concrete.Communication;
+import edu.jhu.hlt.concrete.util.ConcreteException;
+
+/**
+ * @author max
+ *
+ */
+public class TarGzCommunicationIterator implements Iterator<Communication> {
+
+  private final BufferedInputStream bis;
+  private final GzipCompressorInputStream gis;
+  private final TarArchiveInputStream tis;
+  
+  protected final CommunicationSerializer cs = new ThreadSafeCompactCommunicationSerializer();
+  
+  /**
+   * @throws IOException 
+   * 
+   */
+  public TarGzCommunicationIterator(InputStream is) throws IOException {
+    this.bis = new BufferedInputStream(is);
+    this.gis = new GzipCompressorInputStream(bis);
+    this.tis = new TarArchiveInputStream(gis);
+
+    // Prepare next entry.
+    this.tis.getNextTarEntry();
+  }
+
+  @Override
+  public boolean hasNext() {
+    // couple possible states:
+    // processed 1 comm file, and are now on a dir.
+    // processed 1 comm file, and are now on another .comm file.
+    // done iterating (nothing left).
+    
+    // if any entry is null, done; return false.
+    while (this.tis.getCurrentEntry() != null) {
+      String name = this.tis.getCurrentEntry().getName();
+      // no .concrete in name => on a directory
+      // check the next file.
+      if (name.contains(".concrete"))
+        return true;
+      else
+        try {
+          this.tis.getNextEntry();
+        } catch (IOException ioe) {
+          throw new RuntimeException(ioe);
+        }
+    }
+
+    return this.tis.getCurrentEntry() != null;
+  }
+
+  @Override
+  public Communication next() {
+    try {
+      TarArchiveEntry entry = this.tis.getCurrentEntry();
+      String name = entry.getName();
+      if (!name.contains(".concrete")) {
+        // Recurse.
+        this.tis.getNextTarEntry();
+        this.next();
+      }
+      
+      byte[] bytes = IOUtils.toByteArray(tis);
+      this.tis.getNextTarEntry();
+      return this.cs.fromBytes(bytes);
+    } catch (IOException | ConcreteException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  @Override
+  public void remove() {
+    throw new UnsupportedOperationException("You can't remove with this iterator.");
+  }
+}
---
+++ util/src/main/java/edu/jhu/hlt/concrete/serialization/ThreadSafeTarGzCompactCommunicationSerializer.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2012-2014 Johns Hopkins University HLTCOE. All rights reserved.
+ * See LICENSE in the project root directory.
+ */
+package edu.jhu.hlt.concrete.serialization;
+
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Collection;
+import java.util.Iterator;
+
+import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
+import org.apache.commons.compress.archivers.tar.TarArchiveOutputStream;
+import org.apache.commons.compress.compressors.gzip.GzipCompressorOutputStream;
+import org.apache.commons.io.IOUtils;
+
+import edu.jhu.hlt.concrete.Communication;
+import edu.jhu.hlt.concrete.util.ConcreteException;
+
+/**
+ * @author max
+ *
+ */
+public class ThreadSafeTarGzCompactCommunicationSerializer extends ThreadSafeCompactCommunicationSerializer implements CommunicationTarGzSerializer {
+
+  @Override
+  public Iterator<Communication> fromTarGz(InputStream is) throws ConcreteException, IOException {
+    return new TarGzCommunicationIterator(is);
+  }
+
+  @Override
+  public void toTarGz(Collection<Communication> commColl, Path outPath) throws ConcreteException {
+    try(OutputStream os = Files.newOutputStream(outPath);
+        BufferedOutputStream bos = new BufferedOutputStream(os);
+        GzipCompressorOutputStream gzos = new GzipCompressorOutputStream(bos);
+        TarArchiveOutputStream tos = new TarArchiveOutputStream(gzos);) {
+      for (Communication c : commColl) {
+        TarArchiveEntry entry = new TarArchiveEntry(c.getId() + ".concrete");
+        byte[] cbytes = this.toBytes(c);
+        entry.setSize(cbytes.length);
+        tos.putArchiveEntry(entry);
+        try (ByteArrayInputStream bis = new ByteArrayInputStream(cbytes)) {
+          IOUtils.copy(bis, tos);
+          tos.closeArchiveEntry();
+        }
+      }
+      
+    } catch (IOException e) {
+      throw new ConcreteException(e);
+    }
+  }
+
+  @Override
+  public void toTarGz(Collection<Communication> commColl, String outPathString) throws ConcreteException {
+    this.toTarGz(commColl, Paths.get(outPathString));
+  }
+}
