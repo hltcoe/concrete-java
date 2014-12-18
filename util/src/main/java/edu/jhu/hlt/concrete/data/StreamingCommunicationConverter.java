b5ad5d7085d6d1352d61d03c562ee29ba2fb9d99
Max Thomas <maxjthomas@gmail.com>
2014-12-18 16:59:43 -0500

    Make interfaces useful (take an inputstream)

--- util/src/main/java/edu/jhu/hlt/concrete/data/StreamingCommunicationConverter.java
+++ util/src/main/java/edu/jhu/hlt/concrete/data/StreamingCommunicationConverter.java
@@ -5,22 +5,25 @@
 
 package edu.jhu.hlt.concrete.data;
 
+import java.io.InputStream;
 import java.util.Iterator;
 
 import edu.jhu.hlt.concrete.Communication;
 import edu.jhu.hlt.concrete.util.ConcreteException;
 
 /**
- * Tiny interface to standardize more general streaming
- * Concrete conversion tools.
+ * Tiny interface to standardize more general streaming Concrete conversion tools.
  * 
  * @author max
  *
  */
 public interface StreamingCommunicationConverter {
   /**
+   * @param is
+   *          an {@link InputStream} to stream over
    * @return an {@link Iterator} over a stream that produces {@link Communication} objects.
-   * @throws ConcreteException if there is an error processing the stream.
+   * @throws ConcreteException
+   *           if there is an error processing the stream.
    */
-  public Iterator<Communication> iterator() throws ConcreteException;
+  public Iterator<Communication> iterator(InputStream is) throws ConcreteException;
 }
--- util/src/main/java/edu/jhu/hlt/concrete/data/StreamingDocumentableConverter.java
+++ util/src/main/java/edu/jhu/hlt/concrete/data/StreamingDocumentableConverter.java
@@ -5,18 +5,20 @@
 
 package edu.jhu.hlt.concrete.data;
 
+import java.io.InputStream;
 import java.util.Iterator;
 
 /**
- * Tiny interface to standardize more general streaming
- * Concrete conversion tools.
+ * Tiny interface to standardize more general streaming Concrete conversion tools.
  * 
  * @author max
  *
  */
 public interface StreamingDocumentableConverter {
   /**
+   * @param is
+   *          an {@link InputStream} to stream over
    * @return an {@link Iterator} over a stream that produces {@link Documentable} objects.
    */
-  public Iterator<Documentable> iterator();
+  public Iterator<Documentable> iterator(InputStream is);
 }
