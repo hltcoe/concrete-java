05dd54aa6f6f03f9f2ac00166be8bf0e128d0e0e
max thomas <maxjthomas@gmail.com>
2015-01-12 12:47:57 -0500

    Update pom.xml; add an interface for async de/ser.

---
+++ util/src/main/java/edu/jhu/hlt/concrete/serialization/AsyncCommunicationSerializer.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright 2012-2014 Johns Hopkins University HLTCOE. All rights reserved.
+ * See LICENSE in the project root directory.
+ */
+package edu.jhu.hlt.concrete.serialization;
+
+import java.util.concurrent.Future;
+
+import edu.jhu.hlt.concrete.Communication;
+import edu.jhu.hlt.concrete.util.ConcreteException;
+
+/**
+ * @author max
+ *
+ */
+public interface AsyncCommunicationSerializer {
+  public Future<byte[]> toBytes(Communication c) throws ConcreteException;
+  public Future<Communication> fromBytes(byte[] bytes) throws ConcreteException;
+}
