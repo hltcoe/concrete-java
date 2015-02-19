65ed9d0a6c27856d471a5df56cb6f185ec794907
Max Thomas <maxjthomas@gmail.com>
2015-02-18 19:13:34 -0500

    Drop to 4.4.0-SNAPSHOT temporarily. Add main method to CompleteFileIngester.

--- ingesters/simple/src/main/java/edu/jhu/hlt/ingesters/simple/CompleteFileIngester.java
+++ ingesters/simple/src/main/java/edu/jhu/hlt/ingesters/simple/CompleteFileIngester.java
@@ -12,13 +12,18 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Optional;
 
 import org.apache.commons.io.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import edu.jhu.hlt.concrete.Communication;
 import edu.jhu.hlt.concrete.Section;
 import edu.jhu.hlt.concrete.TextSpan;
 import edu.jhu.hlt.concrete.communications.CommunicationFactory;
+import edu.jhu.hlt.concrete.communications.SuperCommunication;
 import edu.jhu.hlt.concrete.ingesters.base.FileIngester;
 import edu.jhu.hlt.concrete.ingesters.base.IngestException;
 import edu.jhu.hlt.concrete.ingesters.base.util.ExistingNonDirectoryFile;
@@ -40,13 +45,13 @@ import edu.jhu.hlt.concrete.util.ConcreteException;
  */
 public class CompleteFileIngester implements FileIngester {
 
-  private final Charset cs;
+  private static final Logger logger = LoggerFactory.getLogger(CompleteFileIngester.class);
 
   /**
-   * Expect UTF-8 encoded documents.
+   *
    */
   public CompleteFileIngester() {
-    this.cs = StandardCharsets.UTF_8;
+
   }
 
   /* (non-Javadoc)
@@ -57,7 +62,7 @@ public class CompleteFileIngester implements FileIngester {
     try {
       ExistingNonDirectoryFile f = new ExistingNonDirectoryFile(path);
       try(InputStream is = Files.newInputStream(f.getPath());) {
-        String content = IOUtils.toString(is, this.cs);
+        String content = IOUtils.toString(is, charset);
         return CommunicationFactory.create(f.getName(), content, "Other");
       } catch (IOException e) {
         throw new IngestException("Caught exception reading in document.", e);
@@ -70,11 +75,90 @@ public class CompleteFileIngester implements FileIngester {
   }
 
   /**
+   * See usage string.
+   *
    * @param args
    */
   public static void main(String[] args) {
-    // TODO Auto-generated method stub
+    if (args.length < 2 || args.length > 3) {
+      System.err.println("This program converts a character-based file to a .concrete file.");
+      System.err.println("The .concrete file will share the same name as the input file.");
+      System.err.println("This program takes 2 or 3 arguments.");
+      System.err.println("Argument 1: path/to/a/character/based/file");
+      System.err.println("Argument 2 (optional): character encoding of document; UTF-8 if omitted");
+      System.err.println("Argument 2/3: path/to/out/concrete/file");
+      System.err.println("Example usage: " + CompleteFileIngester.class.getName()
+          + " /my/text/file UTF-8 /my/output/folder");
+      System.exit(1);
+    }
 
+    String inPathStr = args[0];
+    Path inPath = Paths.get(inPathStr);
+    try {
+      ExistingNonDirectoryFile ef = new ExistingNonDirectoryFile(inPath);
+      Optional<Charset> cs = Optional.empty();
+      Optional<String> outPathStr = Optional.empty();
+
+      if (args.length == 3) {
+        String csStr = args[1];
+        if (!Charset.isSupported(csStr)) {
+          logger.error("Character set {} is not supported.", csStr);
+          System.exit(1);
+        } else {
+          cs = Optional.of(Charset.forName(csStr));
+          outPathStr = Optional.ofNullable(args[2]);
+        }
+      } else {
+        cs = Optional.of(StandardCharsets.UTF_8);
+        outPathStr = Optional.ofNullable(args[1]);
+      }
+
+      Path ep = ef.getPath();
+      String fn = ef.getName();
+      // below will not throw.
+      Path outPath = Paths.get(outPathStr.orElseThrow(() -> new IllegalArgumentException("No output path set.")));
+
+      // Output directory exists, or it doesn't.
+      // Try to create if it does not.
+      if (!Files.exists(outPath)) {
+        try {
+          Files.createDirectories(outPath);
+        } catch (IOException e) {
+          logger.error("Caught exception when making output directories.", e);
+        }
+
+      // if it does, check to make sure it's a directory.
+      } else {
+        if (!Files.isDirectory(outPath)) {
+          logger.error("Output path exists but is not a directory.");
+          System.exit(1);
+        } else {
+          // check to make sure the output file won't be overwritten.
+          Path outFile = outPath.resolve(fn + ".concrete");
+          if (Files.exists(outFile)) {
+            logger.warn("Output file {} exists; not overwriting.", outFile.toString());
+            System.exit(1);
+          }
+
+          // 100 lines of IO error checking to run one line of code
+          try {
+            FileIngester ing = new CompleteFileIngester();
+            Communication comm = ing.fromCharacterBasedFile(ep, cs.get());
+            new SuperCommunication(comm).writeToFile(outFile, false);
+          } catch (IngestException e) {
+            logger.error("Caught exception during ingest.", e);
+            System.exit(1);
+          } catch (ConcreteException e) {
+            logger.error("Caught exception writing output.", e);
+          }
+        }
+      }
+    } catch (NoSuchFileException e) {
+      logger.error("Path {} does not exist.", inPathStr);
+      System.exit(1);
+    } catch (NotFileException e) {
+      logger.error("Path {} is a directory.", inPathStr);
+      System.exit(1);
+    }
   }
-
 }
