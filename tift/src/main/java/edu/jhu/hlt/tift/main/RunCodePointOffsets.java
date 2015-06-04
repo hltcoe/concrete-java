85a5b1f3413d4ae212fcb2bc4f888eb6bec96f48
max thomas <max.thomas@jhu.edu>
2015-06-04 10:43:01 -0400

    Tift: remove string concat, other misc. fixes.

--- tift/src/main/java/edu/jhu/hlt/tift/main/RunCodePointOffsets.java
+++ tift/src/main/java/edu/jhu/hlt/tift/main/RunCodePointOffsets.java
@@ -1,13 +1,15 @@
 /*
- * Copyright 2012-2014 Johns Hopkins University HLTCOE. All rights reserved.
- * This software is released under the 2-clause BSD license.
+ * Copyright 2012-2015 Johns Hopkins University HLTCOE. All rights reserved.
  * See LICENSE in the project root directory.
  */
 package edu.jhu.hlt.tift.main;
 
+import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -19,62 +21,62 @@ import edu.jhu.hlt.tift.Tokenizer;
 
 /**
  * @author max
- * 
+ *
  */
 public class RunCodePointOffsets {
 
-    private static final Logger logger = LoggerFactory.getLogger(RunCodePointOffsets.class);
+  private static final Logger logger = LoggerFactory.getLogger(RunCodePointOffsets.class);
 
-    /**
-     * 
+  /**
+     *
      */
-    public RunCodePointOffsets() {
-        // TODO Auto-generated constructor stub
+  public RunCodePointOffsets() {
+    // TODO Auto-generated constructor stub
+  }
+
+  /**
+   * This function takes 2 arguments:
+   *
+   * <pre>
+   * 1) path to a file of text, and
+   * 2) path to a file of tokens, one per line
+   * </pre>
+   *
+   * This function reads the text into a string, reads the tokens into a List of Strings, then outputs code point offsets for the text-token combination into a
+   * file called "output.txt".
+   *
+   * @param args
+   *          - length 2 array with a path to the original text, and a path to token file, one per line
+   * @throws IOException
+   */
+  public static void main(String[] args) throws IOException {
+    if (args.length != 2) {
+      logger.info("Usage: RunCodePointOffsets [path/to/original/text] [path/to/tokens]");
+      System.exit(1);
     }
 
-    /**
-     * This function takes 2 arguments:
-     * 
-     * <pre>
-     * 1) path to a file of text, and
-     * 2) path to a file of tokens, one per line
-     * </pre>
-     * 
-     * This function reads the text into a string, reads the tokens into a List
-     * of Strings, then outputs code point offsets for the text-token
-     * combination into a file called "output.txt".
-     * 
-     * @param args
-     *            - length 2 array with a path to the original text, and a path
-     *            to token file, one per line
-     * @throws IOException
-     */
-    public static void main(String[] args) throws IOException {
-        if (args.length != 2) {
-            logger.info("Usage: RunCodePointOffsets [path/to/original/text] [path/to/tokens]");
-            System.exit(1);
-        }
+    File inputTextFile = new File(args[0]);
+    File inputTokenFile = new File(args[1]);
 
-        File inputTextFile = new File(args[0]);
-        File inputTokenFile = new File(args[1]);
+    StringBuilder sb = new StringBuilder();
 
-        String text = "";
-        Scanner sc = new Scanner(inputTextFile);
-        while (sc.hasNextLine())
-            text += sc.nextLine();
-        sc.close();
-
-        List<String> tokenList = new ArrayList<>();
-        sc = new Scanner(inputTokenFile);
-        while (sc.hasNextLine())
-            tokenList.add(sc.nextLine());
-        sc.close();
-
-        int[] codePointOffsets = Tokenizer.getOffsets(text, tokenList);
-        FileWriter fw = new FileWriter(new File("output.txt"));
-        for (int i = 0; i < codePointOffsets.length; i++)
-            fw.write(codePointOffsets[i] + "\n");
-
-        fw.close();
+    try (Scanner sc = new Scanner(inputTextFile, StandardCharsets.UTF_8.toString());) {
+      while (sc.hasNextLine())
+        sb.append(sc.nextLine());
     }
+
+    List<String> tokenList = new ArrayList<>();
+    try (Scanner sc = new Scanner(inputTokenFile, StandardCharsets.UTF_8.toString());) {
+      while (sc.hasNextLine())
+        tokenList.add(sc.nextLine());
+    }
+
+    int[] codePointOffsets = Tokenizer.getOffsets(sb.toString(), tokenList);
+    try (BufferedWriter bw = Files.newBufferedWriter(Paths.get("output.txt"), StandardCharsets.UTF_8)) {
+      for (int i = 0; i < codePointOffsets.length; i++) {
+        bw.write(codePointOffsets[i]);
+        bw.write(System.lineSeparator());
+      }
+    }
+  }
 }
