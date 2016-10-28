b2ba8a30974cea0eab750a60a7e3af970fa45ede
Max Thomas <max@maxthomas.io>
2016-10-28 11:48:54 -0400

    Beef up qsub params.

--- redis/src/main/scripts/qsub/tokenizer.sh
+++ redis/src/main/scripts/qsub/tokenizer.sh
@@ -2,8 +2,8 @@
 #$ -j y                        # join stderr to stdout
 #$ -V                          # job has the same environment variables as the submission shell
 #$ -l h_rt=499:00:00            # runtime limit
-#$ -l mem_free=8G             # expected amount of mem
-#$ -l h_vmem=8G             # expected amount of mem
+#$ -l mem_free=16G             # expected amount of mem
+#$ -l h_vmem=16G             # expected amount of mem
 #$ -l num_proc=8
 #$ -o /export/common/max/job-logs/concrete/redis    # log here
 #$ -S /home/hltcoe/mthomas/local/bin/zsh
@@ -12,12 +12,12 @@
 JARP=$1
 java \
     -XX:+UseConcMarkSweepGC \
-    -XX:ParallelCMSThreads=4 \
+    -XX:ParallelCMSThreads=3 \
     -XX:ReservedCodeCacheSize=250M \
     -XX:+TieredCompilation \
     -XX:-UseGCOverheadLimit \
     -XX:+CMSClassUnloadingEnabled \
-    -Xmx3G \
+    -Xmx7G \
     -Dconfig.file=/export/common/max/conf/concrete-redis.conf \
     -Dlog4j.configurationFile=/export/common/max/conf/log4j2-warn.json \
     -cp $JARP \
