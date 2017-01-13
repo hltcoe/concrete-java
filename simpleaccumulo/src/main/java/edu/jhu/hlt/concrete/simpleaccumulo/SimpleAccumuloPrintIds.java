73986bafbcae760bd53dbfeb3aab9fd9b8663a1c
Craig Harman <craig@craigharman.net>
2017-01-13 14:34:59 -0500

    Removed comment that was generating JavaDoc error mentioned in #43

--- simpleaccumulo/src/main/java/edu/jhu/hlt/concrete/simpleaccumulo/SimpleAccumuloPrintIds.java
+++ simpleaccumulo/src/main/java/edu/jhu/hlt/concrete/simpleaccumulo/SimpleAccumuloPrintIds.java
@@ -19,8 +19,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * Simple single-table {@link FetchCommunicationService} using a user-specified column family for isolation.
- *
  * @author travis
  */
 public class SimpleAccumuloPrintIds {
