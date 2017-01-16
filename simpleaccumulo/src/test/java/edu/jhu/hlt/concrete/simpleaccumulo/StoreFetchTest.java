6d461b2e5cbd9d34e8c2c8ba1a3ee0fffb7de41b
Travis Wolfe <twolfe18@gmail.com>
2017-01-16 10:05:01 -0500

    made the FetchStoreTest pass

--- simpleaccumulo/src/test/java/edu/jhu/hlt/concrete/simpleaccumulo/StoreFetchTest.java
+++ simpleaccumulo/src/test/java/edu/jhu/hlt/concrete/simpleaccumulo/StoreFetchTest.java
@@ -5,6 +5,8 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
+import org.apache.accumulo.core.client.security.tokens.PasswordToken;
+
 import edu.jhu.hlt.concrete.AnnotationMetadata;
 import edu.jhu.hlt.concrete.Communication;
 import edu.jhu.hlt.concrete.UUID;
@@ -29,6 +31,19 @@ public class StoreFetchTest {
 
   @Test
   public void storeFetch() throws Exception {
+
+/*
+    Properties p = System.getProperties();
+    if (!p.containsKey("accumulo.user"))
+      throw new RuntimeException("you must provide an accumulo.user");
+    if (!p.containsKey("accumulo.password"))
+      throw new RuntimeException("you must provide an accumulo.password");
+    String user = p.getProperty("accumulo.user");
+    PasswordToken password = new PasswordToken(p.getProperty("accumulo.password"));
+*/
+    String user = "simple_accumulo_writer";
+    PasswordToken password = new PasswordToken("writeTHEdata?!");
+
     SimpleAccumuloConfig config = new SimpleAccumuloConfig(
         "testns",
         SimpleAccumuloConfig.DEFAULT_TABLE,
@@ -37,13 +52,16 @@ public class StoreFetchTest {
     int numThreads = 1;
 
     try (SimpleAccumuloStore store = new SimpleAccumuloStore(config, numThreads)) {
+      store.connect(user, password);
       store.store(comm);
     }
     
     try (SimpleAccumuloFetch fetch = new SimpleAccumuloFetch(config, numThreads)) {
+      fetch.connect(user, password);
       FetchRequest r = new FetchRequest();
       r.addToCommunicationIds(comm.getId());
       FetchResult res = fetch.fetch(r);
+      System.out.println("got back: " + res);
       assertEquals(res.getCommunicationsSize(), 1);
       assertEquals(res.getCommunications().get(0).getText(), comm.getText());
     }
