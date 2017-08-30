af31aa78b117e0fac49fa0cb6f501f6b3c64297d
Max Thomas <max@maxthomas.io>
2017-08-30 08:32:30 -0400

    update to thrift 0.10.0

--- generate.sh
+++ generate.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-CONCRETE_THRIFT_VERSION='0.9.3'
+CONCRETE_THRIFT_VERSION='0.10.0'
 
 AVAILABLE_THRIFT_VERSION=$(thrift -version 2>/dev/null | grep -F "${CONCRETE_THRIFT_VERSION}" | wc -l | sed -e 's/^ *//' -e 's/ *$//')
 if [[ "${AVAILABLE_THRIFT_VERSION}" != '1' ]] ; then
@@ -12,7 +12,7 @@ fi
 
 PATH_TO_THRIFT_FILES=$1
 THRIFT_OUT_DIR='core/src/main/java'
-THRIFT_ARGS="-out $THRIFT_OUT_DIR --gen java:private-members,hashcode,generated_annotations=undated"
+THRIFT_ARGS="-out $THRIFT_OUT_DIR --gen java:private-members,generated_annotations=undated"
 mkdir -p $THRIFT_OUT_DIR
 rm -rf $THRIFT_OUT_DIR/*
 for F in $PATH_TO_THRIFT_FILES/*.thrift; do
