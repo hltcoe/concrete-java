56cc0d3fca998c3b72df7cabf03e10f27ddc2310
Max Thomas <max@maxthomas.io>
2017-09-03 00:22:06 -0400

    bolt: change find command

--- ingesters/bolt/ingest-bolt.sh
+++ ingesters/bolt/ingest-bolt.sh
@@ -2,7 +2,7 @@
 DATA_PATH=$1
 OUTPUT_DIR=$2
 if [[ -d "target" ]]; then
-    find $DATA_PATH -name "bolt*" -type f | xargs -s 2000000 \
+    find $DATA_PATH -name "*.xml" -type f | xargs -s 2000000 \
                                                 java -XX:+UseG1GC -Xmx10G \
                                                 -cp target/*.jar \
                                                 edu.jhu.hlt.concrete.ingesters.bolt.BoltIngesterRunner \
