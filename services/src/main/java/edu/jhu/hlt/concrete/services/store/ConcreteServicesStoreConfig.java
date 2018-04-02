3cf91e1ec0d311db6fe29af822cba607ee3b6601
max thomas <max@maxthomas.io>
2018-04-01 20:56:23 -0400

    store: add javadoc; expose non-standard key for store config

--- services/src/main/java/edu/jhu/hlt/concrete/services/store/ConcreteServicesStoreConfig.java
+++ services/src/main/java/edu/jhu/hlt/concrete/services/store/ConcreteServicesStoreConfig.java
@@ -20,26 +20,80 @@ public class ConcreteServicesStoreConfig extends AbstractHostPortOptionalAuthsCo
 
   private final Config cfg;
 
+  /**
+   * Instantiate a {@link ConcreteServicesStoreConfig} with reasonable defaults.
+   * <br/><br/>
+   * Users bringing a {@link Config} object (e.g. from a config file that includes
+   * a store configuration) should prefer {@link #ConcreteServicesStoreConfig(Config)}.
+   *
+   * @see #ConcreteServicesStoreConfig(Config)
+   */
   public ConcreteServicesStoreConfig() {
     this(ConfigFactory.load());
   }
 
-  public ConcreteServicesStoreConfig(ConcreteServicesConfig cfg) {
-    super(cfg.get().getConfig(CONFIG_STRING));
-    Config store = cfg.get().getConfig(CONFIG_STRING);
-    store.checkValid(ConfigFactory.defaultReference(), CONFIG_STRING);
-    this.cfg = store;
-    LOGGER.debug("Running with config: {}", this.cfg.toString());
-  }
-
+  /**
+   * Instantiate a {@link ConcreteServicesStoreConfig} from a provided {@link Config}.
+   *
+   * @param cfg a {@link Config} object containing a 'store' key with proper configuration values
+   * @see #ConcreteServicesStoreConfig()
+   */
   public ConcreteServicesStoreConfig(Config cfg) {
     this(new ConcreteServicesConfig(cfg));
+  }
+
+  /**
+   * Attempt to load a configuration based off of key 'store' in the config file.
+   * <br/><br/>
+   * Most clients should prefer {@link #ConcreteServicesStoreConfig(Config)} if
+   * bringing a {@link Config} object, or {@link #ConcreteServicesStoreConfig()} if
+   * the defaults are tolerable.
+   *
+   * <br/><br/>
+   * See the project's default in
+   * <code>src/main/resources/reference.conf</code>
+   * for an example.
+   *
+   *
+   * @param cfg a {@link ConcreteServicesStoreConfig} object with a 'store' key
+   *      representing a properly configured store implementation.
+   * @see #ConcreteServicesStoreConfig()
+   * @see #ConcreteServicesStoreConfig(Config)
+   */
+  public ConcreteServicesStoreConfig(ConcreteServicesConfig cfg) {
+    this(cfg, CONFIG_STRING);
+  }
+
+  /**
+   * Load a store configuration from a non-standard configuration key.
+   *
+   * Most clients should prefer {@link #ConcreteServicesStoreConfig(Config)} if
+   * bringing a {@link Config} object, or {@link #ConcreteServicesStoreConfig()}
+   * if the defaults are tolerable.
+   *
+   * @param cfg
+   *          a {@link ConcreteServicesConfig} to use
+   * @param key
+   *          a {@link String} representing the configuration key (e.g.
+   *          'output-store')
+   */
+  public ConcreteServicesStoreConfig(ConcreteServicesConfig cfg, String key) {
+    super(cfg.get().getConfig(key));
+    Config store = cfg.get().getConfig(key);
+    store.checkValid(ConfigFactory.defaultReference(), key);
+    this.cfg = store;
+    LOGGER.debug("Running with config: {}", this.cfg.toString());
   }
 
   public StoreServiceWrapper createWrapper(StoreCommunicationService.Iface impl) throws TException {
     return new StoreServiceWrapper(impl, this.getPort());
   }
 
+  /**
+   * @return a {@link StoreTool} configured based on this configuration
+   * @throws TTransportException
+   *           on failure with connection
+   */
   public StoreTool storeTool() throws TTransportException {
     return new StoreTool(this.getHost(), this.getPort(), this.getAuths());
   }
